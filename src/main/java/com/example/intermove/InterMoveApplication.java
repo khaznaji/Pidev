1f87cb0d4af212ce64006bb242845384b038b65e
achrafbouslama123 <achraf.bouslama@esprit.tn>
2023-02-16 00:05:12 +0100

    first

---
+++ src/main/java/com/example/intermove/InterMoveApplication.java
@@ -0,0 +1,13 @@
+package com.example.intermove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterMoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterMoveApplication.class, args);
+    }
+
+}
